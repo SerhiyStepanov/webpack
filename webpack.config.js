2ab37ed11a4514201c4205c05100e50055418709
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-01 17:20:18 +0200

    del

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
          filename: 'webpack.bundle.js'
-    }
+    },
+    
+    devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 }
